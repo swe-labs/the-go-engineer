204c3c0f61bf8cc9c91a79cf0705195f2380fd7f
Rasel <raselhossen052@gmail.com>
2026-04-28 22:13:02 +0600

    [TEST] add structural tests for exercise main packages (fixes #414)

---
+++ 02-language-basics/4-application-logger/main_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestExercise acts as a structural test to ensure the package builds
+// and to fulfill the curriculum testing requirement for main packages
+// that demonstrate structural or build-time concepts.
+func TestExercise(t *testing.T) {
+	// If the package compiles, the structural test passes.
+}
