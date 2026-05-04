387b262a410961a89d8e5333901f64527dd7587b
Rasel <raselhossen052@gmail.com>
2026-05-05 02:28:02 +0600

    [CHORE] Format go files

--- 11-flagship/01-opslane/internal/handlers/handlers.go
+++ 11-flagship/01-opslane/internal/handlers/handlers.go
@@ -171,8 +171,6 @@ func (app *Application) handleMe(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-
-
 func writeJSON(w http.ResponseWriter, status int, payload any) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
